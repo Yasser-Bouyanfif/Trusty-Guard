45f44844c7acfa28dcab61353e1ca01ebf1fdf7a
Yasser Bouyanfif <yasser.bouyanfif@gmail.com>
2023-06-27 15:51:30 +0200

    3

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -1,7 +1,8 @@
 class Agent < ApplicationRecord
   belongs_to :user
   has_many :availabilities
-  has_and_belongs_to_many :missions
+  has_many :teams_missions
+  has_many :missions, through: :teams_missions
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :user_id, presence: true
