2d80f199ce6ecfeec2d50f101f37f3d19045feba
Yasser Bouyanfif <yasser.bouyanfif@gmail.com>
2023-06-27 15:23:29 +0200

    2

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,2 +1,18 @@
 class MissionsController < ApplicationController
+  def new
+    @mission = Mission.new
+  end
+
+  def create
+    @mission = Mission.new(mission_params)
+    @mission.user = current_user
+    @mission.save!
+    redirect_to root_path
+  end
+
+  private
+
+  def mission_params
+    params.require(:mission).permit(:start_date, :end_date, :number_of_agents, :event_type, :budget, :comment)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :missions, only: [:new, :create, :edit, :update, :destroy]
 end
