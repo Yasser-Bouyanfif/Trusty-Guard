9dedbddd207ed2c8238b8479f564ef434f2d4cad
sofiane flici <sofiane.flici14@gmail.com>
2023-07-01 22:37:56 +0200

    searchBar - Features

--- app/javascript/controllers/search_bar_controller.js
+++ app/javascript/controllers/search_bar_controller.js
@@ -3,20 +3,32 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = [ "form", "list", "searchInput" ]
 
+
   connect() {
+    console.log('hello');
     console.log(this.element);
     console.log(this.formTarget);
     console.log(this.listTarget);
     console.log(this.searchInputTarget);
+
+    console.log('connected');
   }
 
   update() {
-    const url = `${this.formTarget.action}?query=${this.searchInputTarget.value}`
+    const url = `${this.formTarget.action}?query=${this.searchInputTarget.value}`;
     fetch(url, { headers: { 'Accept': 'text/html' } })
       .then(response => response.text())
       .then((data) => {
-        this.listTarget.innerHTML = data;
+        const parser = new DOMParser();
+        const html = parser.parseFromString(data, 'text/html');
+        const table = html.querySelector('.table');
+
+        const existingTable = document.querySelector('.table');
+        existingTable.innerHTML = table.innerHTML;
+
         console.log(data);
       });
   }
+
+
 }
