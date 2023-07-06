ebc2b18ef42e1c95c1e688bc3d5341f3fdf23a20
Yasser Bouyanfif <yasser.bouyanfif@gmail.com>
2023-07-06 11:00:50 +0200

    frooont

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,16 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :dashboard ]
+  skip_before_action :authenticate_user!, only: [ :home, :dashboard, :market ]
 
-  def home
+  def user_profile
+
+  end
+
+  def market
     @missions = Mission.all
     @user = current_user
 
     if current_user
       @estimates = current_user.estimates
     end
-  end
-
-  def user_profile
-    
   end
 end
